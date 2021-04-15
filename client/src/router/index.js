f00774f05e4118137993c5ded81d8f465ea6b955
soniavscott <soniavscott@berkeley.edu>
2021-04-15 15:06:25 -0700

    axios setup

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,19 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Test from "../components/Test.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-  },
+    path: "/test",
+    name: "test",
+    component: Test,
+  }
 ];
 
 const router = new VueRouter({
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/test', (req, res) => {
+  res.send({
+    message: `${req.body.email} has been registered.`
+  })
+})
 
 app.listen(8081)
